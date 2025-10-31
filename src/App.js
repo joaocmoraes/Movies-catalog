d7389b8ec042aa20adfcc4cae8768e0be8b3e51c
João Moraes <115164431+joaocmoraes@users.noreply.github.com>
2025-10-31 18:29:07 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-// src/App.js (Verifique se a ROTA /search existe)
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import DetailsPage from './pages/DetailsPage';
-import SearchPage from './pages/SearchPage'; // Certifique-se de importar
+import SearchPage from './pages/SearchPage'; 
 import Header from './components/Header';
 import BrowsePage from './pages/BrowsePage';
-// ...
+
 
 function App() {
   return (
@@ -15,11 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-
-        {/* ROTA ÚNICA PARA LISTAGEM E FILTROS */}
         <Route path="/browse" element={<BrowsePage />} />
-
-        {/* Mantenha as outras rotas */}
         <Route path="/details/:mediaType/:id" element={<DetailsPage />} />
         <Route path="/search" element={<SearchPage />} />
       </Routes>
